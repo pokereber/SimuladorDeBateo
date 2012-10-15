523514e395372105a9566f5f74f283ca8ebe99b6
chable <robert_dpinio@hotmail.com>
2012-10-15 17:33:39 -0500

    agrege el proyecto en java

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-SimuladorDeBateo
+﻿SimuladorDeBateo
 ================
 
 Este es un sistema de bateo que pretende a manejar la física en aplicaciones de java
+agregar el proyecto en java
---
+++ SimuladorDeBateo/src/simuladordebateo/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladordebateo;
+
+/**
+ *
+ * @author Jorge Dominguez J
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
